af8b008c1ffea90e3522356cf69227c6e18e1bc4
Paul <pauldrdlg@gmail.com>
2022-02-10 17:53:34 -0500

    Diagrammes pour le LA#

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import math as m
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
 if __name__ == "__main__":
-    print("Hello world")
-    print(m.pi)
+    sampleRate, data = wavfile.read('../note_guitare_LAd.wav')
+
+    N = 95  # 44100/466
+
+    data = data[0:95]
+
+    ftd = np.fft.fft(data * np.hanning(N))
+
+    plt.figure()
+    plt.stem(np.absolute(ftd))
+    plt.title("Gain en fonction de m")
+
+    plt.figure()
+    plt.stem(np.angle(ftd))
+    plt.title("Phase en fonction de m")
+
+    plt.show()

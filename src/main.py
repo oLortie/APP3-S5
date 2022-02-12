22b11475ad8458b11ff832668d2077addf75c350
Paul <pauldrdlg@gmail.com>
2022-02-12 17:32:47 -0500

    Début la#, manque le N

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import math as m
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
@@ -11,7 +10,7 @@ def basson():
     N = 6000
     fc = 40
     f0 = 1000
-    K = np.ceil(2*(40*N/sampleRate)+1)
+    K = np.ceil(2*(fc*N/sampleRate)+1)
 
     stopBandTime = []
     w = []
@@ -22,9 +21,7 @@ def basson():
         else:
             stopBandTime.append(-2*((np.sin(np.pi*i*K/N))/(N*np.sin(np.pi*i/N)))*np.cos(2*np.pi*f0/sampleRate*i))
 
-    #y = fct[m.floor(len(fct) / 2) + 1:len(fct)] + fct[0:m.floor(len(fct) / 2) + 1]
-
-    stopBandFreq = np.fft.fft(stopBandTime)
+    # stopBandFreq = np.fft.fft(stopBandTime)
 
     w, h = signal.freqz(stopBandTime)
     fig, ax1 = plt.subplots()
@@ -40,6 +37,8 @@ def basson():
     ax2.axis('tight')
 
     result = np.convolve(data, stopBandTime)
+    for i in range(4):
+        result = np.convolve(result, stopBandTime)
 
     wavfile.write('../note_basson.wav', sampleRate, result.astype(np.int16))
 
@@ -49,25 +48,51 @@ def basson():
 def lad():
     sampleRate, data = wavfile.read('../note_guitare_LAd.wav')
 
-    N = 95  # 44100/466
+    # N = 95  # 44100/466
+    N = 2098  # 2094
 
     data = data[0:95]
 
-    ftd = np.fft.fft(data * np.hanning(N))
+    fc = 22  # pi/1000/2pi*44100
+    K = np.ceil(2 * (fc * N / sampleRate) + 1)
 
-    plt.figure()
-    plt.stem(np.absolute(ftd))
-    plt.title("Gain en fonction de m")
+    x = []
+    lowPassTime = []
+    for i in range(int(-N/2), int(N/2)):
+        x.append(2*np.pi*i/N)
+        if i == 0:
+            lowPassTime.append(K/N)
+        else:
+            lowPassTime.append((np.sin(np.pi*i*K/N))/(N*np.sin(np.pi*i/N)))
 
-    plt.figure()
-    plt.stem(np.angle(ftd))
-    plt.title("Phase en fonction de m")
+    w, h = signal.freqz(lowPassTime)
+    fig, ax1 = plt.subplots()
+    ax1.set_title('Digital filter frequency response')
+    ax1.plot(w, 20 * np.log10(abs(h)), 'b')
+    ax1.set_ylabel('Amplitude [dB]', color='b')
+    ax1.set_xlabel('Frequency [rad/sample]')
+    ax2 = ax1.twinx()
+    angles = np.unwrap(np.angle(h))
+    ax2.plot(w, angles, 'g')
+    ax2.set_ylabel('Angle (radians)', color='g')
+    ax2.grid()
+    ax2.axis('tight')
+
+    # ftd = np.fft.fft(data * np.hanning(N))
+    #
+    # plt.figure()
+    # plt.stem(np.absolute(ftd))
+    # plt.title("Gain en fonction de m")
+    #
+    # plt.figure()
+    # plt.stem(np.angle(ftd))
+    # plt.title("Phase en fonction de m")
 
     return 0
 
 
 if __name__ == "__main__":
-    basson()
-    #lad()
+    # basson()
+    lad()
 
     plt.show()

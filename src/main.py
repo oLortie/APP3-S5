9afd2826277ac0b27026c0a47ab440d890f25d56
Olivier Lortie <olivier.lortie2@usherbrooke.ca>
2022-02-13 11:41:01 -0500

    Init creating notes

--- src/main.py
+++ src/main.py
@@ -45,25 +45,54 @@ def basson():
     return 0
 
 
+def createNote(n, factor, paramFreq, paramMag, paramAngle, env):
+    noteTime = np.zeros(len(env))
+
+    for i in range(len(paramMag)):
+        noteTime = noteTime + paramMag[i] * np.sin(2 * np.pi * paramFreq[i] * factor * n + paramAngle[i])
+
+    #noteTime = noteTime/max(paramMag)
+
+    return noteTime*env
+
+
 def lad():
-    sampleRate, data = wavfile.read('../note_guitare_LAd.wav')
+    #Extraction des paramètres
+    sampleRate, dataTime = wavfile.read('../note_guitare_LAd.wav')
+    N = len(dataTime)
 
-    # N = 95  # 44100/466
-    N = 2098  # 2094
+    dataFreq = np.fft.fft(dataTime * np.hanning(N))
+    dataFreqMag = abs(dataFreq)
+    dataFreqMagDB = 20*np.log10(abs(dataFreq))
+    dataFreqAngle = np.angle(dataFreq)
 
-    data = data[0:95]
+    peaks, properties = signal.find_peaks(dataFreqMagDB, distance=1690)
 
-    fc = 22  # pi/1000/2pi*44100
-    K = np.ceil(2 * (fc * N / sampleRate) + 1)
+    #Amplitudes et phases
+    maxPeaks = 32
 
-    x = []
-    lowPassTime = []
-    for i in range(int(-N/2), int(N/2)):
-        x.append(2*np.pi*i/N)
-        if i == 0:
-            lowPassTime.append(K/N)
-        else:
-            lowPassTime.append((np.sin(np.pi*i*K/N))/(N*np.sin(np.pi*i/N)))
+    paramFreq = []
+    paramMag = []
+    paramAngle = []
+
+    for i in range(maxPeaks):
+        m = peaks[i]
+        paramFreq.append(m*sampleRate/N)
+        paramMag.append(dataFreqMag[m])
+        paramAngle.append(dataFreqAngle[m])
+
+    plt.figure()
+    plt.plot(dataFreqMagDB)
+    plt.title("Gain en fonction de m")
+
+    plt.figure()
+    plt.plot(np.angle(dataFreq))
+    plt.title("Phase en fonction de m")
+
+    #Conception du filtre
+    P = 886  # 886
+
+    lowPassTime = np.full(P, 1/P)
 
     w, h = signal.freqz(lowPassTime)
     fig, ax1 = plt.subplots()
@@ -78,21 +107,27 @@ def lad():
     ax2.grid()
     ax2.axis('tight')
 
-    # ftd = np.fft.fft(data * np.hanning(N))
-    #
-    # plt.figure()
-    # plt.stem(np.absolute(ftd))
-    # plt.title("Gain en fonction de m")
-    #
-    # plt.figure()
-    # plt.stem(np.angle(ftd))
-    # plt.title("Phase en fonction de m")
+    #Extraction de l'enveloppe
+    dataTimeAbs = abs(dataTime)
+    env = np.convolve(dataTimeAbs, lowPassTime)
+
+    n = np.arange(0, N + P - 1, 1)
+    # Recréation du lad
+    ladFactor = 1
+    ladTime = createNote(n, ladFactor, paramFreq, paramMag, paramAngle, env)
+
+    # SOL
+    solFactor = 0.841
+    solTime = createNote(n, solFactor, paramFreq, paramMag, paramAngle, env)
+
+    wavfile.write('../LAD.wav', sampleRate, ladTime.astype(np.int16))
+    wavfile.write('../SOL.wav', sampleRate, solTime.astype(np.int16))
 
     return 0
 
 
 if __name__ == "__main__":
-    # basson()
+    #basson()
     lad()
 
     plt.show()
